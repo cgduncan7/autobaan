dd6ca90a6cc2a7ae3ecfa0e4fcb114071418b036
cgduncan7 <cgduncan7@gmail.com>
2021-11-28 22:34:51 +0100

    Working slowly on adding state machine to perform reservations

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -7,26 +7,10 @@ resource "aws_lambda_function" "reservation_scheduler_lambda" {
   source_code_hash = filebase64sha256("../deploy/reservationScheduler.zip")
 
   role = aws_iam_role.reservation_scheduler_lambda_iam_role.arn
-}
 
-resource "aws_iam_role" "reservation_scheduler_lambda_iam_role" {
-  name = "reservation_scheduler_lambda_iam_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
+  depends_on = [
+    aws_iam_role.reservation_scheduler_lambda_iam_role
   ]
-}
-EOF
 }
 
 resource "aws_lambda_function_event_invoke_config" "reservation_scheduler_lambda_config" {
