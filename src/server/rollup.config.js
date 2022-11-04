9810729617da042c7b03bf300c46386a87cfb0d0
Collin Duncan <collin@klikkie.com>
2022-11-04 14:59:23 +0100

    Removing babel and upgrading rollup to v3

---
+++ src/server/rollup.config.js
@@ -0,0 +1,28 @@
+// rollup.config.js
+import path from 'path'
+
+import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import natives from 'rollup-plugin-natives'
+
+export default {
+  input: path.resolve('src/server/index.ts'),
+  output: {
+    file: './dist/server/index.cjs',
+    format: 'cjs',
+    sourcemap: true,
+  },
+  plugins: [
+    typescript(),
+    natives({
+      copyTo: 'dist/libs',
+      destDir: '../libs',
+      dlopen: false,
+      sourcemap: true,
+      targetEsm: true,
+    }),
+    nodeResolve(),
+    commonjs(),
+  ],
+}
