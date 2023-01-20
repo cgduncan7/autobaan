9f59e6090153bb90c2c93472da4cf984995a2e55
Collin Duncan <3679940+cgduncan7@users.noreply.github.com>
2023-01-20 09:36:37 +0100

    Fixing SQL issues with table creation query

--- src/common/database/sql.ts
+++ src/common/database/sql.ts
@@ -1,8 +1,8 @@
 export const TABLE_reservations = `
-CREATE TABLE reservations (
+CREATE TABLE IF NOT EXISTS reservations (
   id                INT unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
   username          VARCHAR(64) NOT NULL UNIQUE,
-  password          VARCHAR(255) NOT NULL
+  password          VARCHAR(255) NOT NULL,
   date_range_start  DATETIME NOT NULL,
   date_range_end    DATETIME NOT NULL,
   opponent_id       VARCHAR(32) NOT NULL,
