8df792d7bf82dbdda0db3e87e189d6f240820805
Collin Duncan <collin@klikkie.com>
2023-01-29 19:19:08 +0100

    Removing unique constraint from username column

--- src/common/database/sql.ts
+++ src/common/database/sql.ts
@@ -1,7 +1,7 @@
 export const CREATE_TABLE_reservations = `
 CREATE TABLE IF NOT EXISTS reservations (
   id                VARCHAR(36) NOT NULL PRIMARY KEY,
-  username          VARCHAR(64) NOT NULL UNIQUE,
+  username          VARCHAR(64) NOT NULL,
   password          VARCHAR(255) NOT NULL,
   date_range_start  DATETIME NOT NULL,
   date_range_end    DATETIME NOT NULL,
