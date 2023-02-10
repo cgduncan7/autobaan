87e651b38dafcb5a15971e2d68e9aa38481af0d5
Collin Duncan <collin@klikkie.com>
2023-02-10 14:35:25 +0100

    Adding log to custom dayjs wrapper

--- src/common/dayjs.ts
+++ src/common/dayjs.ts
@@ -3,6 +3,7 @@ import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
 import 'dayjs/locale/nl'
+import { asyncLocalStorage } from './logger'
 
 dayjs.extend(isSameOrBefore)
 dayjs.extend(utc)
@@ -14,6 +15,7 @@ dayjs.tz.setDefault('Europe/Amsterdam')
 const dayjsTz = (
   date?: string | number | Date | dayjs.Dayjs | null | undefined
 ) => {
+  asyncLocalStorage.getStore()?.debug(`<dayjs> timezoning ${date}`)
   return dayjs(date).tz()
 }
 
