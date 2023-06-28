962644fb3a38de3cae250f2896a8b0201e9f41f9
Collin Duncan <3679940+cgduncan7@users.noreply.github.com>
2023-06-28 11:26:17 +0200

    Changing env vars to use config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,30 +5,43 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { resolve } from 'path'
 import { ReservationsModule } from './reservations/module'
 import { RunnerModule } from './runner/module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { LoggerMiddleware } from './logger/middleware'
 import { LoggerModule } from './logger/module'
 
 @Module({
 	imports: [
-		TypeOrmModule.forRoot({
-			type: 'sqlite',
-			database: resolve('./db/autobaan_db'),
-			migrations: [resolve('./database/migrations/*.ts')],
-			autoLoadEntities: true,
-			logging: true,
+		ConfigModule.forRoot({ isGlobal: true }),
+		TypeOrmModule.forRootAsync({
+			useFactory: (configService: ConfigService) => ({
+				type: 'sqlite',
+				database: configService.get<string>(
+					'DATABASE',
+					resolve('./db/autobaan_db'),
+				),
+				migrations: [
+					configService.get<string>(
+						'DATABASE_MIGRATIONS',
+						resolve('./database/migrations/*.ts'),
+					),
+				],
+				autoLoadEntities: true,
+				logging: true,
+			}),
 		}),
-		BullModule.forRoot({
-			redis: {
-				host: process.env.REDIS_HOST,
-				port: Number.parseInt(process.env.REDIS_PORT || '6379'),
-			},
-			defaultJobOptions: {
-				removeOnComplete: true,
-			},
+		BullModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: (configService: ConfigService) => ({
+				redis: {
+					host: configService.get<string>('REDIS_HOST', 'localhost'),
+					port: configService.get<number>('REDIS_PORT', 6379),
+				},
+				defaultJobOptions: {
+					removeOnComplete: true,
+				},
+			}),
 		}),
 		ScheduleModule.forRoot(),
-		ConfigModule.forRoot({ isGlobal: true }),
 		ReservationsModule,
 		RunnerModule,
 		LoggerModule,
