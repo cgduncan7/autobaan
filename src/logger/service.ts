014e47e5cfdd1da84871a05314a8ead5913d69af
Collin Duncan <3679940+cgduncan7@users.noreply.github.com>
2023-07-28 19:48:40 +0200

    Changing logger service to be default scope

--- src/logger/service.ts
+++ src/logger/service.ts
@@ -1,6 +1,6 @@
-import { ConsoleLogger, Injectable, Scope } from '@nestjs/common'
+import { ConsoleLogger, Injectable } from '@nestjs/common'
 
-@Injectable({ scope: Scope.REQUEST })
+@Injectable()
 export class LoggerService extends ConsoleLogger {
 	log(message: any, ...optionalParams: any[]) {
 		super.log(message, ...optionalParams)
